6c9e62ae550dd37761fd8805667dafebd638f46a
Dark25 <nadiecaca2000@gmail.com>
2025-05-12 02:52:27 +0200

    chore: update versionCode to 8 and versionName to 1.7.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.anitail.music"
         minSdk = 21
         targetSdk = 36
-        versionCode = 7
-        versionName = "1.6.1"
+        versionCode = 8
+        versionName = "1.7.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
     }
