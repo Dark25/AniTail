b95f17f3c715bd46bbc2c1ac8eb92a671b689363
Dark25 <nadiecaca2000@gmail.com>
2025-05-12 02:54:36 +0200

    chore: update versionName to 1.6.0 in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         minSdk = 21
         targetSdk = 36
         versionCode = 8
-        versionName = "1.7.0"
+        versionName = "1.6.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
     }
